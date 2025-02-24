3f2af528dbf8866b8d8bde100db6626ba3dcd7bb
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-02-24 05:56:33 +0330

    Added: opcodes enumeration structure

--- cholipa.c
+++ cholipa.c
@@ -19,8 +19,57 @@
 
 typedef struct Operand operand_t;
 typedef struct AST ast_node_t;
-typedef int code_t;
 typedef uint32_t tag_t;
+
+typedef enum {
+    OP_LOAD_INT = 0x01,    
+    OP_LOAD_REAL = 0x02,   
+    OP_LOAD_STRING = 0x03, 
+    
+    OP_NEGATE = 0x10,      
+    OP_BITNOT = 0x11,      
+    OP_LENGTH = 0x12,
+    OP_COMPLEMENT = 0x13,
+    
+    OP_ADD = 0x20,         
+    OP_SUB = 0x21,         
+    OP_MUL = 0x22,         
+    OP_DIV = 0x23,         
+    OP_MOD = 0x24,
+
+    OP_SHR = 0x25,
+    OP_SHL = 0x26,
+    OP_BITAND = 0x27,
+    OP_BITOR = 0x28,
+    OP_BITXOR = 0x29,
+
+    OP_DISJ = 0x30,
+    OP_CONJ = 0x31,
+
+    OP_EQ = 0x40,          
+    OP_NE = 0x41,          
+    OP_LT = 0x42,          
+    OP_GT = 0x43,          
+    OP_LE = 0x44,          
+    OP_GE = 0x45,
+
+    OP_LOAD_VAR = 0x50,    
+    OP_STORE_VAR = 0x51,   
+
+    OP_JMP = 0x60,         
+    OP_JMPZ = 0x61,      
+    OP_JMPNZ = 0x62,  
+    
+    OP_CALL = 0x70,
+    OP_TAILCALL = 0x71
+    OP_RETURN = 0x72,      
+
+    OP_INCR = 0x80,
+    OP_DECR = 0x81,
+    
+    OP_HALT = 0xFF         
+} code_t;
+
 
 struct Operand {
   enum OperandType {
@@ -95,8 +144,8 @@ typedef struct BinaryOp {
 } binaryop_t;
 
 typedef struct Label {
-   const char *name;
-   size_t line_no;
+  const char *name;
+  size_t line_no;
 } label_t;
 
 typedef struct AssignVal {
@@ -105,6 +154,11 @@ typedef struct AssignVal {
   ast_node_t **lhs;
   size_t num_lhs;
 } assign_t;
+
+typedef struct DeclareVal {
+  const char **vars;
+  size_t num_vars;
+} decl_t;
 
 typedef struct Closure {
   const char **params;
@@ -210,6 +264,7 @@ struct AST {
     binaryop_t *v_binop;
     unaryop_t *v_unrop;
     assign_t *v_assign;
+    decl_t *v_delcl;
     relop_t *v_relop;
     size_t v_index;
     intmax_t v_integer;
@@ -218,10 +273,34 @@ struct AST {
     const uint8_t *v_string;
   };
 
-  const tag_t tag;
+  tag_t tag;
   bool visited;
   const struct AST *next;
+  const struct AST *prev;
 };
+
+typedef struct Region {
+  size_t size;
+  size_t used;
+  char mem[];
+} region_t;
+
+region_t *new_region(size_t size) {
+  region_t *reg = calloc(1, size);
+  reg->size = size - (sizeof(size_t) * 2);
+  reg->used = 0;
+  return reg;
+}
+
+void *request_memory(region_t *reg, size_t size) {
+  if (reg->size - reg->used < size)
+    RAISE("Memory region exhausted (size: %lu)", reg->size);
+
+  void *mem = (void *)reg->mem[reg->used];
+  reg->used += size + 1;
+
+  return mem;
+}
 
 void init_stacks(void) {
   Stack.operands = calloc(INIT_STACK_SIZE, sizeof(operand_t));
@@ -322,7 +401,7 @@ void set_real_const(long double value, size_t index) {
   Stack.constants[index] = (operand_t){.type = OP_Real, .v_real = value};
 }
 
-void set_string_const(const char *value, size_t index) {
+void set_string_const(const uint8_t *value, size_t index) {
   if (index >= MAX_CONST)
     RAISE("Constant out of range (max %d)", MAX_CONST);
 
@@ -346,3 +425,247 @@ void tape_write_code(code_t code) {
 code_t tape_read_code(void) { return Tape.codes[++Tape.pointer]; }
 
 void tape_set_head(size_t head) { Tape.pointer = head; }
+
+ast_node_t *ast_create_binaryop(ast_node_t *absyn, enum BinaryOperator operator,
+                                bool is_inplace, ast_node_t *lhs,
+                                ast_node_t *rhs) {
+  ast_node_t *new_binop = request_memory(curr_arena, sizeof(ast_node_t));
+
+  new_binop->kind = LEAF_BinaryOp;
+  new_binop->v_binop = request_memory(curr_arena, sizeof(binaryop_t));
+  new_binop->v_binop->operator= operator;
+  new_binop->v_binop->is_inplace = is_inplace;
+  new_binop->v_binop->lhs = lhs;
+  new_binop->v_binop->rhs = rhs;
+
+  ast_append_leaf(absyn, new_binop);
+
+  return new_binop;
+}
+
+ast_node_t *ast_create_relop(ast_node_t *absyn, enum RelOpKind operator,
+                             ast_node_t * lexpr, ast_node_t *rexpr) {
+  ast_node_t *new_relop = request_memory(curr_arena, sizeof(ast_node_t));
+
+  new_relop->kind = LEAF_RelOp;
+  new_relop->v_relop = request_memory(curr_arena, sizeof(relop_t));
+  new_relop->v_relop->operator= operator;
+  new_relop->v_relop->lexpr = lexpr;
+  new_relop->v_relop->rexpr = rexpr;
+
+  ast_append_leaf(absyn, new_relop);
+
+  return new_relop;
+}
+
+ast_node_t *ast_create_unaryop(ast_node_t *absyn, enum UnaryOperator operator,
+                               ast_node_t * operand) {
+  ast_node_t *new_unop = request_memory(curr_arena, sizeof(ast_node_t));
+
+  new_unop->kind = LEAF_UnaryOp;
+  new_unop->v_unrop = request_memory(curr_arena, sizeof(unaryop_t));
+  new_unop->v_unrop->operator= operator;
+  new_unop->v_unrop->operand = operand;
+
+  ast_append_leaf(absyn, new_unop);
+
+  return new_unop;
+}
+
+ast_node_t *ast_create_label(ast_node_t *absyn, const char *name,
+                             size_t line_no) {
+  ast_node_t *new_label = request_memory(curr_arena, sizeof(ast_node_t));
+
+  new_label->kind = LEAF_Label;
+  new_label->v_label = request_memory(curr_arena, sizeof(label_t));
+  new_label->v_label->name = name;
+  new_label->v_label->line_no = line_no;
+
+  ast_append_leaf(absyn, new_label);
+
+  return new_label;
+}
+
+ast_node_t *ast_create_function(ast_node_t *absyn, const char *name,
+                                const char **params, size_t num_params,
+                                ast_node_t *body) {
+  ast_node_t *new_function = request_memory(curr_arena, sizeof(ast_node_t));
+
+  new_function->kind = LEAF_Function;
+  new_function->v_function = request_memory(curr_arena, sizeof(function_t));
+  new_function->v_function->name = name;
+  new_function->v_function->params = params;
+  new_function->v_function->num_params = num_params;
+  new_function->v_function->body = body;
+
+  ast_append_leaf(absyn, new_function);
+
+  return new_function;
+}
+
+cond_pair_t *create_cond_pair(ast_node_t *cond, ast_node_t *body) {
+  cond_pair_t *pair = request_memory(curr_arena, sizeof(cond_pair_t));
+  pair->cond = cond;
+  pair->body = body;
+  return pair;
+}
+
+ast_node_t *ast_create_if_cond(ast_node_t *absyn, cond_pair_t *main_cond,
+                               cond_pair_t *elifs, ast_node_t *els,
+                               bool is_unless) {
+  ast_node_t *new_cond = request_memory(curr_arena, sizeof(ast_node_t));
+
+  new_cond->kind = LEAF_Cond;
+  new_cond->v_cond = request_memory(curr_arena, sizeof(cond_t));
+  new_cond->v_cond->kind = COND_If;
+
+  new_cond->v_cond->v_if = request_memory(curr_arena, sizeof(if_cond_t));
+  new_cond->v_cond->v_if->main = main_cond;
+  new_cond->v_cond->v_if->elifs = elifs;
+  new_cond->v_cond->v_if->els = els;
+  new_cond->v_cond->v_if->is_unless = is_unless;
+
+  ast_append_leaf(absyn, new_cond);
+
+  return new_cond;
+}
+
+ast_node_t *ast_create_switch_cond(ast_node_t *absyn, ast_node_t *discrim,
+                                   cond_pair_t *switches,
+                                   ast_node_t *dfl_case) {
+  ast_node_t *new_cond = request_memory(curr_arena, sizeof(ast_node_t));
+
+  new_cond->kind = LEAF_Cond;
+  new_cond->v_cond = request_memory(curr_arena, sizeof(cond_t));
+  new_cond->v_cond->kind = COND_Switch;
+
+  new_cond->v_cond->v_switch =
+      request_memory(curr_arena, sizeof(switch_cond_t));
+  new_cond->v_cond->v_switch->discrim = discrim;
+  new_cond->v_cond->v_switch->switches = switches;
+  new_cond->v_cond->v_switch->dfl_case = dfl_case;
+
+  ast_append_leaf(absyn, new_cond);
+
+  return new_cond;
+}
+
+ast_node_t *ast_create_loop(ast_node_t *absyn, enum LoopKind kind,
+                            ast_node_t *start, ast_node_t *end,
+                            ast_node_t *step, ast_node_t *body) {
+  ast_node_t *new_loop = request_memory(curr_arena, sizeof(ast_node_t));
+
+  new_loop->kind = LEAF_Loop;
+  new_loop->v_loop = request_memory(curr_arena, sizeof(loop_t));
+  new_loop->v_loop->kind = kind;
+  new_loop->v_loop->start = start;
+  new_loop->v_loop->end = end;
+  new_loop->v_loop->step = step;
+  new_loop->v_loop->body = body;
+
+  ast_append_leaf(absyn, new_loop);
+
+  return new_loop;
+}
+
+ast_node_t *ast_create_call(ast_node_t *absyn, const char *prefix_name,
+                            const ast_node_t **arguments, size_t num_args,
+                            bool is_tail) {
+  ast_node_t *new_call = request_memory(curr_arena, sizeof(ast_node_t));
+
+  new_call->kind = LEAF_Call;
+  new_call->v_call = request_memory(curr_arena, sizeof(call_t));
+  new_call->v_call->prefix_name = prefix_name;
+  new_call->v_call->arguments = arguments;
+  new_call->v_call->num_args = num_args;
+  new_call->v_call->is_tail = is_tail;
+
+  ast_append_leaf(absyn, new_call);
+
+  return new_call;
+}
+
+ast_node_t *ast_create_declare(ast_node_t *absyn, const char **vars,
+                               size_t num_vars) {
+  ast_node_t *new_declare = request_memory(curr_arena, sizeof(ast_node_t));
+
+  new_declare->kind = LEAF_DeclareVal;
+  new_declare->v_delcl = request_memory(curr_arena, sizeof(decl_t));
+  new_declare->v_delcl->vars = vars;
+  new_declare->v_delcl->num_vars = num_vars;
+
+  ast_append_leaf(absyn, new_declare);
+
+  return new_declare;
+}
+
+ast_node_t *ast_create_assign(ast_node_t *absyn, const char **rhs,
+                              size_t num_rhs, ast_node_t **lhs,
+                              size_t num_lhs) {
+  ast_node_t *new_assign = request_memory(curr_arena, sizeof(ast_node_t));
+
+  new_assign->kind = LEAF_AssignVal;
+  new_assign->v_assign = request_memory(curr_arena, sizeof(assign_t));
+  new_assign->rhs = rhs;
+  new_assign->rhs_num = rhs_num;
+  new_assign->lhs = lhs;
+  new_assign->lhs_num = lhs_num;
+
+  ast_append_leaf(absyn, new_assign);
+
+  return new_assign;
+}
+
+ast_node_t *ast_create_integer(ast_node_t *absyn, intmax_t value) {
+  ast_node_t *new_int = request_memory(curr_arena, sizeof(ast_node_t));
+
+  new_int->kind = LEAF_Integer;
+  new_int->v_integer = value;
+
+  ast_append_leaf(absyn, new_int);
+
+  return new_int;
+}
+
+ast_node_t *ast_create_real(ast_node_t *absyn, long double value) {
+  ast_node_t *new_real = request_memory(curr_arena, sizeof(ast_node_t));
+
+  new_real->kind = LEAF_Real;
+  new_real->v_real = value;
+
+  ast_append_leaf(absyn, new_real);
+
+  return new_real;
+}
+
+ast_node_t *ast_create_real(ast_node_t *absyn, long double value) {
+  ast_node_t *new_real = request_memory(curr_arena, sizeof(ast_node_t));
+
+  new_real->kind = LEAF_Real;
+  new_real->v_real = value;
+
+  ast_append_leaf(absyn, new_real);
+
+  return new_real;
+}
+
+void ast_append_leaf(ast_node_t *absyn, ast_node_t *new_leaf) {
+  if (absyn == NULL) {
+    return;
+  }
+
+  if (absyn->next == NULL) {
+    absyn->next = new_leaf;
+    new_leaf->prev = absyn;
+  } else {
+    ast_node_t *last = absyn;
+    while (last->next != NULL) {
+      last = last->next;
+    }
+
+    last->next = new_leaf;
+    new_leaf->prev = last;
+  }
+
+  new_leaf->visited = false;
+}
